1f3494cd950dc2626a1e12b3fe824e704d0ecab4
Odos Matthews <odosmatthews.gmail.com>
2018-09-05 08:19:06 -0400

    first commit

---
+++ helloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FirstConsoleProject
+{
+    class MainClass
+    {
+        public static void Main (string[] args) // this is a method called "Main"
+        {
+            Console.WhiteLine("Hello World!");
+        }
+    }
+}
